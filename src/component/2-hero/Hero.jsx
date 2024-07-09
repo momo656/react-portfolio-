b8a66da120bcc1539ff01d69e46bb1dabb2bda03
Mohamed Abdelsalam <68692823+momo656@users.noreply.github.com>
2024-07-09 18:15:09 +0300

    Update Hero.jsx

--- src/component/2-hero/Hero.jsx
+++ src/component/2-hero/Hero.jsx
@@ -3,25 +3,25 @@ import Lottie from "lottie-react";
 import mainAnimation from "../../../public/animation/main.json";
 export default function Hero() {
   return (
-    <section className="hero flex">
+    <section id="hero" className="hero flex">
       <div className="left-section">
         <div className="parent-avatar flex">
-          <img src="./me.png" className="avatar" alt="" />
+          <img src="./public/me.png" className="avatar" alt="" />
           <div className="icon-verified"></div>
         </div>
         <h1 className="title">Software designer.</h1>
         <p className="sub-title">
-          I’m Mohamed Abdelsalm, a software designer. I specialize in crafting
+          I’m Mohamed Abdelsalm, a software designer . I specialize in crafting
           impactful websites for small and medium-sized businesses. From custom
           designs to responsive e-commerce solutions, I provide tailored
           services to enhance your online presence and drive results.
         </p>
 
         <div className="all-icons flex">
-          <div className="icon icon-twitter"><a href="https://www.facebook.com/"><a/></div>
-          <div className="icon icon-instagram"><a href="https://www.instagram.com/"><a/></div>
-          <div className="icon icon-github"><a href="https://github.com/momo656"><a/></div>
-          <div className="icon icon-linkedin"><a href="https://www.linkedin.com/in/mohamed-abdelsalam-982995182"><a/></div>
+          <a className="icon " href=""><div className="icon icon-twitter"></div></a>
+          <a  className="icon" href=""><div className="icon icon-instagram"></div></a>
+          <a  className="icon" href="https://github.com/momo656"><div  className="icon icon-github"></div></a>
+          <a  className="icon" href="https://www.linkedin.com/in/mohamed-abdelsalam-982995182/"><div className="icon icon-linkedin"></div></a>
         </div>
       </div>
       <div className="right-section"><Lottie
